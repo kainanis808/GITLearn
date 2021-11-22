df333fab61fa696e8ef3f29b4f442535f44ab05d
A Trimble, Director RIP Lab <atrimble@hawaii.edu>
2021-11-22 13:31:05 -1000

    Added simple dynamics, preliminary work on dynamic parameters

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # ME 696 Fall 2021: GIT Learn
 Super Simple Repository for learning git/github for ME696
 
+- Most code contained in this repository is poorly commented and should not be used as representative of good coding practice
+- Most code contatined in this repository is for a specific case and is not very robust
+
 ## Directory Descriptions
 - [HoverCraft](./HoverCraft), Contains a Matlab file solution to the MIT Hover Craft problem.
 - [ROSPackages](./ROSPackages), Contains custom ROS packages developed for the class
---
+++ ROSPackages/Simulations/me696_turtlesim/src/dynamics/mithc.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+class MITHoverCraftDynamics:
+
+    def __init__(self): 
+       self.dt = 1.0/10.0
+       self.nu0 =  {'u':0, 'v':0, 'r':0}
+       self.nu1 = self.nu0
+       self.tau =  {'X':0, 'Y':0, 'N':0}
+
+    def set_dt(self,dt):
+        self.dt = dt
+
+    def set_tau(self,tau):
+        self.tau = tau
+
+    def update_nu(self):
+        self.nu1['u'] = self.nu0['u']*(1-self.dt) + self.dt*self.tau['X']
+        self.nu1['v'] = self.nu0['v'] + self.dt*self.tau['Y']
+        self.nu1['r'] = self.nu0['r'] + self.dt*self.tau['N']
+
+        self.nu0 = self.nu1
+
+        return self.nu0
